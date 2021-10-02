643b77a02c141342636334b00cfdcd7d2822bce6
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-10-02 12:33:25 +0200

    Create new mapping, change others mapping, UserController

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,14 +1,18 @@
 package org.example.controllers;
 
-import org.example.domain.ToDo;
 import org.example.domain.User;
+import org.example.domain.plainObject.UserPojo;
 import org.example.services.inrerfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 /**
  * by Iskortsev S.V.
@@ -24,13 +28,21 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping(path = "/user")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
-        return new ResponseEntity<>(user, HttpStatus.OK);
+    @PostMapping(path = "/user/create")
+    public ResponseEntity<UserPojo> createUser(@RequestBody User user) {
+        UserPojo result = userService.createUser(user);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
-    @PostMapping(path = "/todo")
-    public ResponseEntity<ToDo> createToDo(@RequestBody ToDo todo) {
-        return new ResponseEntity<>(todo, HttpStatus.OK);
+    @GetMapping(path = "/user/{id}")
+    public ResponseEntity<UserPojo> getUser(@PathVariable Long id) {
+        UserPojo result = userService.getUser(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/user/all")
+    public ResponseEntity<List<UserPojo>> getUser() {
+        List<UserPojo> result = userService.getAllUsers();
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
