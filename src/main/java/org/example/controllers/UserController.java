cecdaf4fe2ddcf78dbd576c4facc9c2bb0ba136d
Stanislav Iskortsev <iskortsevsv@gmail.com>
2021-10-02 20:46:26 +0200

    Add exception handler at Controllers

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -2,14 +2,20 @@ package org.example.controllers;
 
 import org.example.domain.User;
 import org.example.domain.plainObject.UserPojo;
+import org.example.exceptions.CustomEmptyDataException;
 import org.example.services.inrerfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * by Iskortsev S.V.
@@ -52,6 +58,51 @@ public class UserController {
     @DeleteMapping(path = "/user/{id}")
     public ResponseEntity<String> deleteUser (@PathVariable Long id) {
         return new ResponseEntity<>(userService.deleteUser(id), HttpStatus.OK);
+    }
+
+    /**
+     * Exception handling
+     * */
+
+    //конфликт имен
+    @ExceptionHandler
+    public ResponseEntity<String> onConflictUsername(DataIntegrityViolationException exception) {
+        //можно вписать в систему логирования
+        return ResponseEntity.status(HttpStatus.CONFLICT) // в теле получим имя класса
+                .body(ClassUtils.getShortName(exception.getClass()) + ": User with such name already registered");
+    }
+    //попытка обратится к несуществующему пользователю
+    @ExceptionHandler
+    public ResponseEntity<String> onMissingUserId(NoSuchElementException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ClassUtils.getShortName(exception.getClass()) + ": No such user was found");
+    }
+
+    //ошибка доступа к пустой базе
+    @ExceptionHandler
+    public ResponseEntity<String> onMissingUser (EmptyResultDataAccessException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ClassUtils.getShortName(exception.getClass())
+                + exception.getLocalizedMessage()
+                + ": no one user was found");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> SQLProblems (SQLException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ClassUtils.getShortName(exception.getClass())
+                + exception.getSQLState()
+                + exception.getLocalizedMessage()
+                + ": something went wrong with user");
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> customExceptionHandler (CustomEmptyDataException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ClassUtils.getShortName(exception.getClass())
+                + " "
+                + exception.getCause()
+                + " "
+                + exception.getLocalizedMessage());
     }
 
 }
